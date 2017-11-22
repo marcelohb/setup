a7b82a21d79d036c6d1adde646e3dbb67fa47dd3
marcelobattaglion <marcelo.battaglion@beblue.com.br>
2017-11-22 10:32:49 -0200

    Inclui -y em alguns apt-get

--- setup.sh
+++ setup.sh
@@ -6,10 +6,10 @@ apt-get update
 apt-get install -y openjdk-8-jdk
 
 echo INSTALL GIT
-apt-get install git
+apt-get install git -y
 
 echo INSTALL DOCKER CE
-sudo apt-get install docker-compose
+sudo apt-get install docker-compose -y
 #Pertencer a root
 sudo groupadd docker
 sudo usermod -aG docker $USER
@@ -27,20 +27,19 @@ echo INSTALL SUBLIME
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
-sudo apt-get install sublime-text
+sudo apt-get install sublime-text -y
 
 echo INSTALL GITKRAKEN
-wget https://www.gitkraken.com/download/linux-deb
+wget -o gitkraken.deb https://www.gitkraken.com/download/linux-deb
 sudo dpkg -i gitkraken-amd64.deb
 
 echo INSTALL VLC
-sudo apt-get install vlc
+sudo apt-get install vlc -y
 
 echo INSTALL GOOGLE CHROME
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update ; sudo apt-get install google-chrome-stable -y
-
 
 echo INSTALL DROPBOX
 wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
@@ -51,10 +50,10 @@ wget https://mega.nz/linux/MEGAsync/xUbuntu_17.10/amd64/megasync-xUbuntu_17.10_a
 sudo dpkg -i megasync-xUbuntu_17.10_amd64.deb
 
 echo INSTALL GPARTED
-sudo apt-get install gparted
+sudo apt-get install gparted -y
 
 echo INSTALL MYSQL WORKBENCH
-sudo apt-get install mysql-workbench
+sudo apt-get install mysql-workbench -y
 
 echo INSTALL FASTOREDIS
 wget http://fastoredis.com/download_linux_p.html?platform=linux&filename=fastoredis-1.9.1-x86_64.deb
@@ -77,9 +76,9 @@ unzip -d /opt/gradle gradle-4.3.1-bin.zip
 ls /opt/gradle/gradle-4.3.1
 
 echo INSTALL GRAFICOS
-sudo apt-get install gimp
-sudo apt-get install pinta
-sudo apt-get install kazam
+sudo apt-get install gimp -y
+sudo apt-get install pinta -y
+sudo apt-get install kazam -y
 
 echo INSTALL SPOTIFY
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
@@ -87,45 +86,6 @@ echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sourc
 sudo apt-get update
 sudo apt-get install spotify-client
 
+sudo apt-get autoremove
 
-#sudo apt-get install \
-#    linux-image-extra-$(uname -r) \
-#    linux-image-extra-virtual -y
-#
-#sudo apt-get update
-#
-#sudo apt-get install \
-#    apt-transport-https \
-#    ca-certificates \
-#    curl \
-#    software-properties-common -y
-#
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#
-#sudo apt-key fingerprint 0EBFCD88
-
-#sudo add-apt-repository \
-#   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-#   $(lsb_release -cs) \
-#   stable"
-#
-#sudo apt-get update
-
-#sudo apt-get install docker-ce
-
-
-#$ sudo apt-get install \
-#    apt-transport-https \
-#    ca-certificates \
-#    curl \
-#    software-properties-common
-#echo ADD DOCKER KEY
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#echo fingerprint
-#sudo apt-key fingerprint 0EBFCD88
-#
-#echo AMD64 DOCKER VERSION
-#sudo add-apt-repository \
-#   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-#   $(lsb_release -cs) \
-#   stable"
+sudo apt --fix-broken install
